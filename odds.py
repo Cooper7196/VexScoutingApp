d6a4e01439110050c6d95ddd65cdc77dcff94ca4
Cooper7196 <cbrosgall@shaw.ca>
2023-04-25 22:37:39 -0500

    Simply predicted wins calculations

--- odds.py
+++ odds.py
@@ -8,7 +8,9 @@ def sim_match(odds):
     return random.randint(0, 1000) < odds * 10
 
 def get_odds(matches):
-    # matches = [54.1, 100-65.3, 100-50.4, 59.8, 78.0, 76.5, 53.3, 53.1, 100-62.1, 56.1, 54.4]
+    # print("avg", sum(matches) / 100)
+    return sum(matches) / 100
+    """
     res = []
     for i in range(NUMSIMS):
         tournament = []
@@ -21,11 +23,12 @@ def get_odds(matches):
         counts[tournament.count(True)] += 1
     odds = [count / NUMSIMS * 100 for count in counts]
 
-    sum = 0
+    winSum = 0
     total = 0
     for num, odd in enumerate(odds):
-        sum += num * odd
+        winSum += num * odd
         total += odd
 
     # print(f"{sum / total:.2f} average wins")
-    return sum / total
+    return winSum / total
+    """
